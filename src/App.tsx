1f83f3446d1358a2dc03364a726fd36b41953d52
Jade Fish <high5132@outlook.com>
2025-04-13 14:59:04 +0800

    dev: add sqlite3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {FlashCardPage} from './FlashCard';
 import {Home} from './Home';
 import "./App.css";
+import { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/core';
 function App() {
+  const [initSucc, setInitSucc] = useState<Boolean>(true);
+  const [reason, setReason] = useState<String>("No reason");
+  useEffect(()=>{
+    invoke<null>('greet')
+      .catch((s:String) => {
+        setReason(s);
+        setInitSucc(false);
+      });
+  });
+  if (!initSucc) {
+    return (
+      <h1>Failed to init, {reason}</h1>
+    )
+  }
   return (
     <main className="container">
       <BrowserRouter>
